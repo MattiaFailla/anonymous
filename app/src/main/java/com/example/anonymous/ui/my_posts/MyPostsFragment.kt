b50cad33567da59f593ed76a7e6e343b8f6d5ba8
Mattia Failla <mattiafailla@hotmail.it>
2021-10-06 16:06:11 +0200

    Improve app bar navigation menu

--- app/src/main/java/com/example/anonymous/ui/my_posts/MyPostsFragment.kt
+++ app/src/main/java/com/example/anonymous/ui/my_posts/MyPostsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.anonymous.ui.gallery
+package com.example.anonymous.ui.my_posts
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.anonymous.R
-import com.example.anonymous.databinding.FragmentGalleryBinding
+import com.example.anonymous.databinding.FragmentMyPostsBinding
 
-class GalleryFragment : Fragment() {
+class MyPostsFragment : Fragment() {
 
-    private lateinit var galleryViewModel: GalleryViewModel
-    private var _binding: FragmentGalleryBinding? = null
+    private lateinit var myPostsViewModel: MyPostsViewModel
+    private var _binding: FragmentMyPostsBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +24,14 @@ class GalleryFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+        myPostsViewModel =
+            ViewModelProvider(this).get(MyPostsViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentMyPostsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        galleryViewModel.text.observe(viewLifecycleOwner, Observer {
+        val textView: TextView = binding.textMyPosts
+        myPostsViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- app/src/main/java/com/example/anonymous/ui/my_posts/MyPostsViewModel.kt
+++ app/src/main/java/com/example/anonymous/ui/my_posts/MyPostsViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.anonymous.ui.gallery
+package com.example.anonymous.ui.my_posts
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class GalleryViewModel : ViewModel() {
+class MyPostsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is gallery Fragment"
+        value = "Qui appaiono tutti i post che ho pubblicato"
     }
     val text: LiveData<String> = _text
 }
